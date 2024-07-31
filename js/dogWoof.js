49e964dbb44c1ff29a039e099892ec34f981cbe4
Max-Myanovskiy <myanovskiy.m@gmail.com>
2024-07-31 15:34:25 +0300

    added some code

--- js/dogWoof.js
+++ js/dogWoof.js
@@ -13,6 +13,8 @@ bark("fido", "20");
 bark("dino", 0, 0);
 bark("scottie", -1);
 
+// ==============================
+
 function whatShallIWear(temp) {
   if (temp < 60) {
     console.log("Wear a jacket");
@@ -27,6 +29,8 @@ whatShallIWear(50);
 whatShallIWear(80);
 whatShallIWear(60);
 
+// ==============================
+
 function calculateArea(r) {
   var area;
   if (r <= 0) {
@@ -39,3 +43,54 @@ function calculateArea(r) {
 var radius = 6.2;
 var theArea = calculateArea(radius);
 console.log("The area is: " + theArea);
+
+// ==============================
+
+function youSayGoodBye() {
+  alert("Good Bye!");
+  function andISayHello() {
+    alert("Hello!");
+  }
+  return andISayHello;
+}
+
+let something = youSayGoodBye();
+something();
+
+// ==============================
+
+function stopWatch() {
+  let startTime = Date.now();
+  function getDelay() {
+    let elapsedTime = Date.now() - startTime;
+    alert(elapsedTime);
+  }
+  return getDelay;
+}
+
+let timer = stopWatch();
+
+// Сделать что-нибудь за некоторое время.
+for (let i = 0; i < 1000000; i++) {
+  let foo = Math.random() * 10000;
+}
+
+timer();
+
+// ==============================
+
+// let thingToPrint = "";
+// function drawText() {
+//   thingToPrint += "*";
+//   document.writeln(thingToPrint);
+// }
+// let intervalID = setInterval(drawText, 2000);
+//   clearInterval(intervalID);
+
+// ==============================
+
+let myButton = document.querySelector("#myButton");
+myButton.addEventListener("click", doSomething, false);
+function doSomething(e) {
+  alert("Is this working?");
+}
